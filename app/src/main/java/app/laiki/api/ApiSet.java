5258e70aa62926d770d612b4dc2aeb82bd097fad
F.Gvozdev <f.gvozdev@gmail.com>
2018-06-19 19:40:46 +0300

    Privacy Policy

--- app/src/main/java/app/laiki/api/ApiSet.java
+++ app/src/main/java/app/laiki/api/ApiSet.java
@@ -1,13 +1,15 @@
 package app.laiki.api;
 
 public enum  ApiSet {
-    TEST("http://laiki.app/api/", true),
-    PROD("https://laiki.app/api/", false);
+    TEST("http://laiki.app", "http://laiki.app/api/", true),
+    PROD("https://laiki.app", "https://laiki.app/api/", false);
 
+    public final String webUrl;
     public final String baseUrl;
     public final boolean fixSsl;
 
-    ApiSet(String baseUrl, boolean fixSsl) {
+    ApiSet(String webUrl, String baseUrl, boolean fixSsl) {
+        this.webUrl = webUrl;
         this.baseUrl = baseUrl;
         this.fixSsl = fixSsl;
     }
--- app/src/main/java/app/laiki/ui/login/LoginPage1IntroViewHolder.java
+++ app/src/main/java/app/laiki/ui/login/LoginPage1IntroViewHolder.java
@@ -1,6 +1,12 @@
 package app.laiki.ui.login;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -12,19 +18,48 @@ import butterknife.OnClick;
 import app.laiki.R;
 import app.laiki.utils.Utils;
 
+import static app.laiki.App.prefs;
+
 public class LoginPage1IntroViewHolder implements LoginActivity.LoginPageViewHolder {
 
     private final View root;
     @BindView(R.id.button) TextView button;
+    @BindView(R.id.policy) TextView policy;
 
     public LoginPage1IntroViewHolder(View root) {
         this.root = root;
         ButterKnife.bind(this, root);
+        policy.setText(buildLicenceText());
+        policy.setFocusable(true);
+        policy.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @OnClick(R.id.button)
     public void onViewClicked() {
         LoginActivity activity = (LoginActivity) Utils.getActivity(root);
         activity.onContinue();
+    }
+
+    private SpannableStringBuilder buildLicenceText() {
+        String t1 = "Продолжая, вы соглашаетсясь с ";
+        String t2 = "политикой конфиденциальности";
+        SpannableStringBuilder message = new SpannableStringBuilder(t1 + t2);
+
+        ClickableSpan licence = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Activity activity = Utils.getActivity(policy);
+                if (activity == null)
+                    return;
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(prefs().getApiSet().webUrl + "/privacy"));
+                activity.startActivity(intent);
+            }
+        };
+        message.setSpan(licence, t1.length(), t1.length() + t2.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+        return message;
     }
 }
