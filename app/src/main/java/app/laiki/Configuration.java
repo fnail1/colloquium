6c084a48c94040a1fb1a2751826a130858777f60
F.Gvozdev <f.gvozdev@gmail.com>
2018-06-26 18:54:05 +0300

    Предрелизные правки

--- app/src/main/java/app/laiki/Configuration.java
+++ app/src/main/java/app/laiki/Configuration.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Configuration implements Serializable {
     public final Notifications notifications = new Notifications();
-    public int questionsFrameSize = BuildConfig.DEBUG ? 2 : 15;
+    public int questionsFrameSize = BuildConfig.DEBUG ? 2 : 12;
     public int deadTime = BuildConfig.DEBUG ? 30 * 1000 : 60 * 60 * 1000;
 
     public static class Notifications {
